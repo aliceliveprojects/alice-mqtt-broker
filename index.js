0b3997a7284327a3374f08275279802834cd3def
alicedigitallabs <32162302+AliceDigitalLabs@users.noreply.github.com>
2020-03-06 08:01:34 +0000

    changed mqtt server resource to aedes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var http = require('http'),
-    httpServ = http.createServer(),
-    mosca = require('mosca'),
-    mqttServ = new mosca.Server({});
+const aedes = require('./aedes')()
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+const port = process.env.PORT || 8080;
 
-mqttServ.attachHttpServer(httpServ);
+ws.createServer({ server: httpServer }, aedes.handle)
 
-httpServ.listen(process.env.PORT || 8080);
+httpServer.listen(port, function () {
+  console.log('websocket server listening on port ', port)
+})
