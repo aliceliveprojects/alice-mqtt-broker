8b17673f24fb6258dea36c61f7d8bec2603d5bab
alicedigitallabs <32162302+AliceDigitalLabs@users.noreply.github.com>
2020-03-06 08:57:44 +0000

    added persistence, but local changes cause errors. just testing on remote.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const aedes = require('aedes')()
+
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
+const persistence = require('aedes-persistence')()
+const aedes = require('aedes')()
+
 const port = process.env.PORT || 8080;
 
 ws.createServer({ server: httpServer }, aedes.handle)
