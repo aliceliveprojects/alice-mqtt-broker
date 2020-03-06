473ab97a45c95a44bfccf025402d3661af3ee520
alicedigitallabs <32162302+AliceDigitalLabs@users.noreply.github.com>
2020-03-06 08:09:42 +0000

    fixed typo?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const aedes = require('./aedes')()
+const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
 const port = process.env.PORT || 8080;
